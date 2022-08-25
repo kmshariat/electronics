0257c6d625d122ee6fa401df92c8b35535fccf3f
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-08-25 08:57:00 +0600

    create d2b and b2d

---
+++ number_system_conversion.py
@@ -0,0 +1,15 @@
+#decimal to binary number conversion using recursion
+def d2b(number):
+    if(number > 1):
+        d2b(number//2)
+    print(number%2, end=' ')
+
+#binary to decimal number conversion using while loop
+def b2d(number):
+  decimal, i = 0, 0
+  while(number != 0):
+    dec = number % 10
+    decimal = decimal + dec * pow(2, i)
+    number = number//10
+    i += 1
+  print(decimal)
