07e35158ecd5efb6270f2cba99aed277f1088365
K.M Shariat Ullah <cast.shariat@gmail.com>
2022-09-08 15:51:06 +0600

    Update currentDivision.py

--- currentDivision.py
+++ currentDivision.py
@@ -9,14 +9,14 @@ with schemdraw.Drawing() as d:
     d.config(inches_per_unit=.5, unit=3, color = 'yellow')
     d += (A := elm.Dot().label('A'))
     d += (node1 := elm.Line().right(d.unit/2).dot())
-    d += elm.Resistor().right().label('$2a \Omega$')
+    d += elm.Resistor().right().label('$1/2a \Omega$')
     d += elm.Line().up(d.unit/2).at(node1.end)
-    d += elm.Resistor().right().label('$a \Omega$')
+    d += elm.Resistor().right().label('$1/a \Omega$')
     d += elm.Line().down(d.unit/2).dot()
     d += elm.Line().right(d.unit/2)
     d += (B := elm.Dot().label('B'))
     d += elm.Line().down(d.unit/2).at(node1.end) 
-    d += elm.Resistor().right().label('$3a \Omega$')
+    d += elm.Resistor().right().label('$1/3a \Omega$')
     d += elm.Line().up(d.unit/2)
 
     #save
